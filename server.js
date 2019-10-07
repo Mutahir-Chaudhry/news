49ea8c22d1bcbaa5ae28f566fb4c38d87ecef87b
Mutahir-Chaudhry <mutachaudhry@gmail.com>
2019-10-07 13:30:38 -0400

    Added express-handlebars

--- server.js
+++ server.js
@@ -1,9 +1,22 @@
-var express = require ("express");
+var express = require("express");
+var expressHandlebars = require("express-handlebars");
+var bodyParser = require("body-parser");
+
 var PORT = process.env.PORT || 3000;
 var app = express();
 var router = express.Router();
 
 app.use(express.static(__dirname + "/public"));
+
+app.engine("handlebars", expressHandlebars({
+    defaultLayout: "main"
+}));
+app.set("view engine", "handlebars");
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
 app.use(router);
 
 app.listen(PORT, function(){
