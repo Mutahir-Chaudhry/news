5c271728d580a09138e3750de750980f1c93770b
Mutahir-Chaudhry <mutachaudhry@gmail.com>
2019-10-07 13:37:29 -0400

    Added MongoDB connection

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var expressHandlebars = require("express-handlebars");
 var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
 
 var PORT = process.env.PORT || 3000;
 var app = express();
@@ -18,6 +19,15 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(router);
+
+var db = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+mongoose.connect(db, function(error){
+    if (error){
+        console.log(error);
+    } else {
+        console.log("Mongoose connection is successful!")
+    }
+})
 
 app.listen(PORT, function(){
     console.log("Listening on port:" + PORT);
