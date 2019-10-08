018285c0ed59c7f0359db2deafd5de2fa9257abe
Mutahir-Chaudhry <mutachaudhry@gmail.com>
2019-10-08 12:21:13 -0400

    Created get root route and get saved route

---
+++ config/routes.js
@@ -0,0 +1,8 @@
+module.export = function(router) {
+    router.get("/", function(req, res) {
+        res.render("home");
+    });
+    router.get("/saved", function(req,res){
+        res.render("saved");
+    });
+}
