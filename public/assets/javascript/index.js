82f575bfe04072c53d44d0a24d5659c11afc6213
Mutahir-Chaudhry <mutachaudhry@gmail.com>
2019-10-08 13:39:04 -0400

    Added initpage function, renderarticles function, and renderempty()

---
+++ public/assets/javascript/index.js
@@ -0,0 +1,49 @@
+$(document).ready(function(){
+    var articleContainer = $(".article-container");
+
+    $(document).on("click", ".btn.save", handleArticleSave);
+    $(document).on("click", ".scrape-new", handleArticleSave);
+
+    initPage();
+
+    function initPage() {
+        articleContainer.empty();
+        $.get("/api/headlines?saved=false")
+        .then(function(data){
+            if (data && data.length){
+                renderArticles(data);
+            }
+            else {
+                renderEmpty();
+            }
+        });
+    }
+
+    function renderArticles(articles){
+        var articlePanels = [];
+
+        for (var i = 0; i < articles.length; i++) {
+            articlePanels.push(createPanel(articles[i]));
+        }
+    }
+    
+    function renderEmpty(){
+        var emptyAlert = 
+        $(["<div class='alert aler-warning text-center'>",
+    "<h4>Uh Oh, Looks like we don't have any new articles :(</h4>",
+"</div>",
+"<div class='panel panel-default'>",
+"<div class='panel-heading text-center'>",
+"<h3>What would you like to do?</h3>",
+"</div>",
+"<div class='panel-body text-center'>",
+"<h4><a class='scrpae-new'>Try scraping new articles!</a></h4>",
+"<h4><a href='saved'>Go to Saved Articles</a></h4>",
+"</div>",
+"</div>"
+].join(""));
+articleContainer.append(emptyAlert);
+    }
+
+    
+})
